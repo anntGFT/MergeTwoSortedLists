32b4c3277d068aeeae8c4cbf1d9e8e3b06050267
anntGFT <109505145+anntGFT@users.noreply.github.com>
2022-11-23 15:58:51 +0100

    Update Solution.java

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -64,12 +64,12 @@ public class Solution {
 
         if( list1.val < list2.val){
 
-            list1.next = mergeTwoLists(list1.next, list2);
+            list1.next = mergeTwoLists2(list1.next, list2);
 
             return list1;
         }else{
 
-            list2.next = mergeTwoLists(list1, list2.next);
+            list2.next = mergeTwoLists2(list1, list2.next);
 
             return list2;
         }
